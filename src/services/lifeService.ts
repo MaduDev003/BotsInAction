4a1ffaabdeaec2bf56705d8cde35b14e52cbaf68
MaduDev003 <mariabevenuto062@gmail.com>
2026-05-13 13:14:30 -0300

    fix: import path

--- src/services/lifeService.ts
+++ src/services/lifeService.ts
@@ -1,7 +1,8 @@
 
 import { gameOver } from "./gameService";
-import redHeart from '../assets/images/redHeart.png';
 import blackHeart from '../assets/images/blackHeart.png';
+import redHeart from '../assets/images/redHeart.png';
+
 
 let currentLives = 4;
 
